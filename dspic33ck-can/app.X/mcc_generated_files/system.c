e436c38a2328e9d73534e084c4a227630526836c
c14050 <roy.bean@microchip.com>
2022-03-08 23:54:43 -0700

    Correcting clock configuration range

--- dspic33ck-can/app.X/mcc_generated_files/system.c
+++ dspic33ck-can/app.X/mcc_generated_files/system.c
@@ -66,7 +66,7 @@
 #pragma config OSCIOFNC = ON    //OSC2 Pin Function bit->OSC2 is general purpose digital I/O pin
 #pragma config FCKSM = CSDCMD    //Clock Switching Mode bits->Both Clock switching and Fail-safe Clock Monitor are disabled
 #pragma config PLLKEN = ON    //PLL Lock Status Control->PLL lock signal will be used to disable PLL clock output if lock is lost
-#pragma config XTCFG = G3    //XT Config->24-32 MHz crystals
+#pragma config XTCFG = G1    //XT Config->8-16 MHz crystals
 #pragma config XTBST = ENABLE    //XT Boost->Boost the kick-start
 
 // FWDT
